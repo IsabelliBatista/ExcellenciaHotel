090d50c152a0cb0a005452c05460dc68ec7db07c
Caio Mariano <caioms@nc7i.com.br>
2021-11-25 13:59:31 -0300

    adicionando páginas de perfil

---
+++ PIM_/PIM_/Controllers/DadosPessoaisController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PIM_.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIM_.Controllers
+{
+    public class DadosPessoaisController : Controller
+    {
+        private readonly ILogger<DadosPessoaisController> _logger;
+
+        /* public FavoritosController(ILogger<DadosPessoaisController> logger)
+         {
+             _logger = logger;
+         } */
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       /* [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+    }
+}
---
+++ PIM_/PIM_/Controllers/DetalhesQuartoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PIM_.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIM_.Controllers
+{
+    public class DetalhesQuartoController : Controller
+    {
+        private readonly ILogger<DetalhesQuartoController> _logger;
+
+        /* public FavoritosController(ILogger<DetalhesQuartoController> logger)
+         {
+             _logger = logger;
+         } */
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       /* [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+    }
+}
